f0c2056abe3fd83c213e812645dc7feea570a643
Malyshev Alexander <_undead_@mail.ru>
2024-05-22 01:36:29 +1000

    routes - базовый набор путей для api и web

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ApartmentsController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::controller( ApartmentsController::class )->prefix( 'apartments' )->group( function( ) {
+	Route::get( '/', 'index' );
+	Route::get( '/{apartment}', 'instance' );
+	Route::get( '/{apartment}/prices', 'prices' );
+} );
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\ApartmentsController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::controller( PagesController::class )->group( function( ) {
+	Route::get( '/', 'main' )->name( 'page_main' );
+	Route::get( '/apartments', 'apartments' )->name( 'page_apartments' );
+	Route::get( '/apartments/add', 'newApartment' );
+	Route::get( '/apartments/{apartment}', 'editApartment' );
+	Route::get( '/reservs', 'reservs' )->name( 'page_reservs' );
+	Route::get( '/customers', 'customers' )->name( 'page_customers' );
+	Route::get( '/inventories', 'inventories' )->name( 'page_inventories' );
+} );
+
+Route::controller( ApartmentsController::class )->group( function( ) {
+	Route::post( '/apartments', 'add' );
+	Route::put( '/apartments/{apartment}', 'update' );
+} );
