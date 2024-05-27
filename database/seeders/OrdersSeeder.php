e17c1faf0d8b7394dec4fdaba444d85de6e8daaa
Malyshev Alexander <_undead_@mail.ru>
2024-05-28 00:43:10 +1000

    seeders - сиды для наполнения рыбой

--- database/seeders/OrdersSeeder.php
+++ database/seeders/OrdersSeeder.php
@@ -6,21 +6,27 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Apartment;
 use Illuminate\Database\Eloquent\Factories\Sequence;
+use App\Models\Order;
+use App\Models\Customer;
 
-class ApartmentsSeeder extends Seeder {
-	use WithoutModelEvents;
-	
+class OrdersSeeder extends Seeder {
 	/**
 	 * Run the database seeds.
 	 */
 	public function run( ) : void {
-		Apartment::factory( )
+		$apartments = Apartment::factory( )
 			->hasPrices( 3 )
 			->count( 20 )
 			->state( new Sequence( fn( Sequence $sequence ) => [
 				'title' => 'База 1, домик #'.$sequence->index + 1,
 				'number' => $sequence->index + 1
 			] ) )
+			->create( );
+		
+		Order::factory( )
+			->count( 60 )
+			->recycle( $apartments )
+			->has( Customer::factory( ) )
 			->create( );
 	}
 }
