7be34d87366b918c3a832baf4bc597dadc8e04c7
Malyshev Alexander <_undead_@mail.ru>
2024-06-04 00:28:13 +1000

    seeders - inventories, фиксированный набор инвентаря

--- database/seeders/InventoriesSeeder.php
+++ database/seeders/InventoriesSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Inventory;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 
 class InventoriesSeeder extends Seeder {
 	use WithoutModelEvents;
@@ -13,6 +14,12 @@ class InventoriesSeeder extends Seeder {
 	 * Run the database seeds.
 	 */
 	public function run( ) : void {
-		Inventory::factory( )->count( 10 )->hasPrices( 3 )->create( );
+		$titles = [ 'Мангал', 'Чайник', 'Кружка', 'Вилка', 'Тарелка', 'Одеяло', 'Плед' ];
+		
+		Inventory::factory( )
+			->hasPrices( 2 )
+			->count( count( $titles ) )
+			->sequence( fn( Sequence $sequence ) => [ 'title' => $titles[ $sequence->index ] ] )
+			->create( );
 	}
 }
