8822651b4e89ccc95dc8f5692e5a770a771b85c6
Malyshev Alexander <_undead_@mail.ru>
2024-06-05 00:03:36 +1000

    seeders - orders, упрощенная версия для тестирования

--- database/seeders/OrdersSeeder.php
+++ database/seeders/OrdersSeeder.php
@@ -7,8 +7,6 @@ use Illuminate\Database\Seeder;
 use App\Models\Apartment;
 use App\Models\Order;
 use App\Models\Customer;
-use App\Models\Inventory;
-use Illuminate\Database\Eloquent\Factories\Sequence;
 
 class OrdersSeeder extends Seeder {
 	use WithoutModelEvents;
@@ -17,26 +15,10 @@ class OrdersSeeder extends Seeder {
 	 * Run the database seeds.
 	 */
 	public function run( ) : void {
-		$this->call( [ ApartmentsSeeder::class, CustomersSeeder::class ] );
-		
-		$apartments = Apartment::all( );
-		
-		$apartments->each( function( Apartment $apartment ) {
-			Order::factory( )
-				->for( $apartment )
-				->for( $apartment->currentPrice )
-				->hasPayments( 2 )
-				->hasAttached(
-					Inventory::factory( )->hasPrices( 2 )->count( 2 ),
-					fn( ) => [ 'comment' => fake( )->text( ) ]
-				)
-				->count( 1 )
-				->sequence( fn( Sequence $sequence ) => [
-					'customer_id' => Customer::all( )->random( ),
-					'from' => fake( )->dateTimeBetween( '-1 week', '+1 week' ),
-					'to' => fake( )->dateTimeBetween( '+2 week', '+3 week' )
-				] )
-				->create( );
-		} );
+		Order::factory( )
+			->has( Apartment::factory( ) )
+			->has( Customer::factory( ) )
+			->count( 60 )
+			->create( );
 	}
 }
